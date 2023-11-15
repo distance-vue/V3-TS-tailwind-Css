4c097312157e57abcd2988a566b904b120fc4e88
sh <ylmskj21@gmail.com>
2023-11-15 16:08:31 +0800

    css

---
+++ src/views/Home/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main container mx-auto text-center">
+    <p class="text-cyan-500 cursor-pointer">home</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
